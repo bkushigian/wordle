a363c4a6d468be77eb0e127acb83c56d0d744c0a
bkushigian <bkushigian@gmail.com>
2024-10-30 21:59:58 -0700

    CLI and words

--- wordle/cli.py
+++ wordle/cli.py
@@ -16,20 +16,22 @@ def print_guess_result(guess, hint):
 def main():
     w = Wordle()
     guesses = []
+    hints = []
     guess = ''
     while w.is_running():
-        for g in guesses:
-            print(f"       {print_guess_result(g)}")
+        for h, g in zip(hints, guesses):
+            print(f"       {print_guess_result(g, h)}")
         guess = input("\033[94;1mGuess> \033[0m")
         try:
-            guesses.append(w.guess(guess))
+            hints.append(w.guess(guess))
+            guesses.append(guess)
 
         except RuntimeError as e:
             print(f"       \033[31;1m{str(e)}")
             if str(e).lower() == "game over":
                 break
-    for g in guesses:
-        print(f"       {print_guess_result(g)}")
+    for h, g in zip(hints, guesses):
+        print(f"       {print_guess_result(g, h)}")
     if guess == w.word:
         print(f"\033[1;94mCongratulations!\033[0m You found the word \033[32;1m{w.word} \033[0m"
               f"in \033[32;1m{len(w.guesses)} tries\033[0m!!")
